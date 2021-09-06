a983b833d224df8bb9d407e4f53efd49b32c3301
Craig <craigjope@gmail.com>
2021-09-06 14:59:27 -0600

    pulling in index.html on root but not accessing any .js files

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I change app paragraph gh-pages settings is main
         </p>
         <a
           className="App-link"
